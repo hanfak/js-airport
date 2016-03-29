d8804454581d48557915538d88e2bf824e3a5928
Hanif Fakira <fakira.work@gmail.com>
2016-03-29 12:06:11 +0100

    put in fizz

--- jasmine/spec/FizzBuzzSpec.js
+++ jasmine/spec/FizzBuzzSpec.js
@@ -3,16 +3,19 @@ describe("FizzBuzz", function() {
   var fizzbuzz;
 
   beforeEach(function(){
+    fizzbuzz = new FizzBuzz();
     for (i=1; i < 101; i++) {
       array.push(i);
     }
-
-
   });
 
-  it('prints numbers 1-100', function() {
-    fizzbuzz = new FizzBuzz();
-    expect(fizzbuzz.play()).toEqual(array);   
+  it('returns an array of 100 elements', function() {
+    expect(fizzbuzz.play().length).toEqual(100);
+  });
+
+  it('prints Fizz for any number divisible by 3', function(){
+    expect(fizzbuzz.play()[2]).toEqual('Fizz');
+
   });
 
 
